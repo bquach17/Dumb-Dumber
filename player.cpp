8f4bbe6d2f09d40f93df2b6b0c7f19ec23a6940a
mangoQueen <ycchung@caltech.edu>
2018-03-02 20:52:24 -0800

    everything commit

--- player.cpp
+++ player.cpp
@@ -39,9 +39,7 @@ Player::~Player() {
  * return nullptr.
  */
 Move *Player::doMove(Move *opponentsMove, int msLeft) {
-    /*
-     * TODO: Implement how moves your AI should play here. You should first
-     * process the opponent's opponents move before calculating your own move
-     */
+
+    
     return nullptr;
 }
